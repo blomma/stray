4d94b39641f10401d520a482099ce70ecbee124d
Mikael Hultgren <blomma@gmail.com>
2012-05-25 17:22:31 +0200

    Added datamodel

---
+++ Drift/Event.h
@@ -0,0 +1,19 @@
+//
+//  Event.h
+//  Drift
+//
+//  Created by Mikael Hultgren on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Event : NSManagedObject
+
+@property (nonatomic) NSDate * startDate;
+@property (nonatomic) NSDate * stopDate;
+@property (nonatomic) NSString * tag;
+
+@end
---
+++ Drift/Event.m
@@ -0,0 +1,18 @@
+//
+//  Event.m
+//  Drift
+//
+//  Created by Mikael Hultgren on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Event.h"
+
+
+@implementation Event
+
+@dynamic startDate;
+@dynamic stopDate;
+@dynamic tag;
+
+@end
