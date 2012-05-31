58244240cb909af4f59170426f31f931ff03a309
Mikael Hultgren <blomma@gmail.com>
2012-05-31 07:26:17 +0200

    And one subclass for the other view also

---
+++ Drift/TimerArchiveViewController.m
@@ -0,0 +1,43 @@
+//
+//  TimerArchiveViewController.m
+//  Drift
+//
+//  Created by Mikael Hultgren on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TimerArchiveViewController.h"
+
+@interface TimerArchiveViewController ()
+
+@end
+
+@implementation TimerArchiveViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
