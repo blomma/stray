717f86533f03161fa90e8b529587c949453fa5db
Mikael Hultgren <blomma@gmail.com>
2012-05-31 07:24:56 +0200

    Added subclasses for the views

---
+++ Drift/TimerViewController.m
@@ -0,0 +1,43 @@
+//
+//  TimerViewController.m
+//  Drift
+//
+//  Created by Mikael Hultgren on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TimerViewController.h"
+
+@interface TimerViewController ()
+
+@end
+
+@implementation TimerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
