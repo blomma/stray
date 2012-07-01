000e464f130d515d397e034c4e60a32418a37245
Mikael Hultgren <blomma@gmail.com>
2012-07-01 15:43:18 +0200

    Added running time to the model

--- Drift/_Event.m
+++ Drift/_Event.m
@@ -5,6 +5,10 @@
 
 const struct EventAttributes EventAttributes = {
 	.running = @"running",
+	.runningTimeHours = @"runningTimeHours",
+	.runningTimeMilliseconds = @"runningTimeMilliseconds",
+	.runningTimeMinutes = @"runningTimeMinutes",
+	.runningTimeSeconds = @"runningTimeSeconds",
 	.startDate = @"startDate",
 	.stopDate = @"stopDate",
 	.tag = @"tag",
@@ -46,6 +50,22 @@ const struct EventFetchedProperties EventFetchedProperties = {
 		NSSet *affectingKey = [NSSet setWithObject:@"running"];
 		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
 	}
+	if ([key isEqualToString:@"runningTimeHoursValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"runningTimeHours"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"runningTimeMillisecondsValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"runningTimeMilliseconds"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"runningTimeMinutesValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"runningTimeMinutes"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
+	if ([key isEqualToString:@"runningTimeSecondsValue"]) {
+		NSSet *affectingKey = [NSSet setWithObject:@"runningTimeSeconds"];
+		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
+	}
 
 	return keyPaths;
 }
@@ -73,6 +93,110 @@ const struct EventFetchedProperties EventFetchedProperties = {
 
 - (void)setPrimitiveRunningValue:(BOOL)value_ {
 	[self setPrimitiveRunning:[NSNumber numberWithBool:value_]];
+}
+
+
+
+
+
+@dynamic runningTimeHours;
+
+
+
+- (int64_t)runningTimeHoursValue {
+	NSNumber *result = [self runningTimeHours];
+	return [result longLongValue];
+}
+
+- (void)setRunningTimeHoursValue:(int64_t)value_ {
+	[self setRunningTimeHours:[NSNumber numberWithLongLong:value_]];
+}
+
+- (int64_t)primitiveRunningTimeHoursValue {
+	NSNumber *result = [self primitiveRunningTimeHours];
+	return [result longLongValue];
+}
+
+- (void)setPrimitiveRunningTimeHoursValue:(int64_t)value_ {
+	[self setPrimitiveRunningTimeHours:[NSNumber numberWithLongLong:value_]];
+}
+
+
+
+
+
+@dynamic runningTimeMilliseconds;
+
+
+
+- (int16_t)runningTimeMillisecondsValue {
+	NSNumber *result = [self runningTimeMilliseconds];
+	return [result shortValue];
+}
+
+- (void)setRunningTimeMillisecondsValue:(int16_t)value_ {
+	[self setRunningTimeMilliseconds:[NSNumber numberWithShort:value_]];
+}
+
+- (int16_t)primitiveRunningTimeMillisecondsValue {
+	NSNumber *result = [self primitiveRunningTimeMilliseconds];
+	return [result shortValue];
+}
+
+- (void)setPrimitiveRunningTimeMillisecondsValue:(int16_t)value_ {
+	[self setPrimitiveRunningTimeMilliseconds:[NSNumber numberWithShort:value_]];
+}
+
+
+
+
+
+@dynamic runningTimeMinutes;
+
+
+
+- (int16_t)runningTimeMinutesValue {
+	NSNumber *result = [self runningTimeMinutes];
+	return [result shortValue];
+}
+
+- (void)setRunningTimeMinutesValue:(int16_t)value_ {
+	[self setRunningTimeMinutes:[NSNumber numberWithShort:value_]];
+}
+
+- (int16_t)primitiveRunningTimeMinutesValue {
+	NSNumber *result = [self primitiveRunningTimeMinutes];
+	return [result shortValue];
+}
+
+- (void)setPrimitiveRunningTimeMinutesValue:(int16_t)value_ {
+	[self setPrimitiveRunningTimeMinutes:[NSNumber numberWithShort:value_]];
+}
+
+
+
+
+
+@dynamic runningTimeSeconds;
+
+
+
+- (int16_t)runningTimeSecondsValue {
+	NSNumber *result = [self runningTimeSeconds];
+	return [result shortValue];
+}
+
+- (void)setRunningTimeSecondsValue:(int16_t)value_ {
+	[self setRunningTimeSeconds:[NSNumber numberWithShort:value_]];
+}
+
+- (int16_t)primitiveRunningTimeSecondsValue {
+	NSNumber *result = [self primitiveRunningTimeSeconds];
+	return [result shortValue];
+}
+
+- (void)setPrimitiveRunningTimeSecondsValue:(int16_t)value_ {
+	[self setPrimitiveRunningTimeSeconds:[NSNumber numberWithShort:value_]];
 }
 
 
