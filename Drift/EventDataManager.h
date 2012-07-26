d3ab8cfe1b71954c664b8847c2e5fb2628669209
Mikael Hultgren <blomma@gmail.com>
2012-07-26 02:56:27 +0200

    Removed from project

---
+++ Drift/EventDataManager.h
@@ -0,0 +1,13 @@
+//
+//  ModelManager.h
+//  Drift
+//
+//  Created by Mikael Hultgren on 7/25/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModelManager : NSObject
+
+@end
---
+++ Drift/EventDataManager.m
@@ -0,0 +1,13 @@
+//
+//  ModelManager.m
+//  Drift
+//
+//  Created by Mikael Hultgren on 7/25/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import "ModelManager.h"
+
+@implementation ModelManager
+
+@end
