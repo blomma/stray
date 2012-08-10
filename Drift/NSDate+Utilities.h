2d17944e4543c8a6ec2092e7b75fcfaceb9f26dd
Mikael Hultgren <blomma@gmail.com>
2012-08-11 00:55:01 +0200

    Renamed and recoded
    Made use of components instead to calculate the start of the day

---
+++ Drift/NSDate+Utilities.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+Utilities.h
+//  Drift
+//
+//  Created by Mikael Hultgren on 8/1/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Utilities)
+
+- (NSDate *)beginningOfDay;
+- (NSDate *)endOfDay;
+
+@end
---
+++ Drift/NSDate+Utilities.m
@@ -0,0 +1,34 @@
+//
+//  NSDate+Utilities.m
+//  Drift
+//
+//  Created by Mikael Hultgren on 8/1/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import "NSDate+Utilities.h"
+
+@implementation NSDate (Utilities)
+
+- (NSDate *)beginningOfDay {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    // Get the weekday component of the current date
+	NSDateComponents *components = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit)
+											   fromDate:self];
+	return [calendar dateFromComponents:components];
+}
+
+- (NSDate *)endOfDay {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+	NSDateComponents *componentsToAdd = [[NSDateComponents alloc] init];
+	// to get the end of day for a particular date, add 1 day
+	[componentsToAdd setDay:1];
+
+	NSDate *endOfDay = [calendar dateByAddingComponents:componentsToAdd
+												 toDate:[self beginningOfDay]
+												options:0];
+
+	return endOfDay;
+}
+
+@end
