1108878f39dc7db4507668c53b21b9d8417fe2d3
Mikael Hultgren <blomma@gmail.com>
2012-08-13 23:31:24 +0200

    Removed todo
    Wasn't using it

---
+++ Drift/EventGroupChange.h
@@ -0,0 +1,13 @@
+//
+//  EventGroupChange.h
+//  Drift
+//
+//  Created by Mikael Hultgren on 8/13/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventGroupChange : NSObject
+
+@end
---
+++ Drift/EventGroupChange.m
@@ -0,0 +1,13 @@
+//
+//  EventGroupChange.m
+//  Drift
+//
+//  Created by Mikael Hultgren on 8/13/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import "EventGroupChange.h"
+
+@implementation EventGroupChange
+
+@end
