d1c03d0016dc1b087dd805806a583e8bceb448c2
Mikael Hultgren <blomma@gmail.com>
2012-08-13 23:33:23 +0200

    Added convenient defines

--- Drift/NSDate+Utilities.h
+++ Drift/NSDate+Utilities.h
@@ -8,6 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+#define D_MINUTE	60
+#define D_HOUR		3600
+#define D_DAY		86400
+#define D_WEEK		604800
+#define D_YEAR		31556926
+
 @interface NSDate (Utilities)
 
 - (NSDate *)beginningOfDay;
