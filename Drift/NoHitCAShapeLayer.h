48b2568c2f5aa6b31ab14c079a602ab2e503f243
Mikael Hultgren <blomma@gmail.com>
2012-08-26 17:09:42 +0200

    Unused auto commit from xcode

---
+++ Drift/NoHitCAShapeLayer.h
@@ -0,0 +1,13 @@
+//
+//  NoHitCAShapeLayer.h
+//  Drift
+//
+//  Created by Mikael Hultgren on 8/26/12.
+//  Copyright (c) 2012 Artsoftheinsane. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface NoHitCAShapeLayer : CAShapeLayer
+
+@end
