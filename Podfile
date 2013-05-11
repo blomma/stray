96d7674f0d7610edf1569e4f50d6d875d9c47ad0
Mikael Hultgren <blomma@gmail.com>
2013-05-11 14:07:30 +0200

    Updated pod and Crashlytics

--- Podfile
+++ Podfile
@@ -1,4 +1,5 @@
 platform :ios, '6.0'
+inhibit_all_warnings!
 
 pod 'MagicalRecord',  '~> 2.1.0'
 pod 'GoogleAnalytics-iOS-SDK', '~> 2.0Beta4'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,21 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GoogleAnalytics-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 2.0beta4.
+
+// MagicalRecord
+#define COCOAPODS_POD_AVAILABLE_MagicalRecord
+#define COCOAPODS_VERSION_MAJOR_MagicalRecord 2
+#define COCOAPODS_VERSION_MINOR_MagicalRecord 1
+#define COCOAPODS_VERSION_PATCH_MagicalRecord 0
+
