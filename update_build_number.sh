5b2f97981588f9014bc87d7470f6939457f9d7d0
Mikael Hultgren <blomma@gmail.com>
2014-09-13 19:30:41 +0200

    Calculate the correct build number
    the build number is the last commit count on master + any branch points
    from it

--- update_build_number.sh
+++ update_build_number.sh
@@ -1,4 +1,4 @@
 branch=${1:-'master'}
-buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
+buildNumber=$(expr $(git rev-list $branch --count) + $(git rev-list $branch..HEAD --count))
 echo "Updating build number to $buildNumber using branch '$branch'."
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
