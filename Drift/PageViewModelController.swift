233862746d5b801dc7c995a380d236993e8f7ccf
Mikael Hultgren <blomma@gmail.com>
2014-10-09 18:33:23 +0200

    Converted RootViewController to swift and a UIView

---
+++ Drift/PageViewModelController.swift
@@ -0,0 +1,73 @@
+//
+//  PageViewModelController.swift
+//  Drift
+//
+//  Created by Mikael Hultgren on 2014-10-08.
+//  Copyright (c) 2014 Artsoftheinsane. All rights reserved.
+//
+
+import UIKit
+
+class PageViewModelController: NSObject, UIPageViewControllerDataSource {
+    let pageData : [String]
+    
+    override init() {
+        pageData = [
+            "EventViewController",
+            "EventStatisticsViewController"
+        ]
+        
+        super.init()
+    }
+    
+    func viewControllerAtIndex(index: Int, storyboard: UIStoryboard) -> UIViewController? {
+        if (pageData.count == 0) || (index >= pageData.count) {
+            return nil
+        }
+        
+        let controller = storyboard.instantiateViewControllerWithIdentifier(pageData[index]) as UIViewController
+        return controller
+    }
+    
+    func indexOfViewController(viewController: UIViewController) -> Int? {
+        if let identifier: String = viewController.restorationIdentifier {
+            if let i = find(pageData, identifier) {
+                return i
+            }
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page View Controller Data Source
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        if let index = indexOfViewController(viewController) {
+            if index > 0 {
+                var beforeIndex = index - 1
+                
+                return viewControllerAtIndex(beforeIndex, storyboard: viewController.storyboard!)
+            }
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        if let index = indexOfViewController(viewController) {
+            var afterIndex = index + 1
+            if afterIndex < pageData.count {
+                return viewControllerAtIndex(afterIndex, storyboard: viewController.storyboard!)
+            }
+        }
+        
+        return nil
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pageData.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+}
