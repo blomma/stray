fc7ab8d4bfd776fba308d120fc11695eaef8ab16
Mikael Hultgren <blomma@gmail.com>
2014-10-15 20:52:57 +0200

    Removed extra whitespace

--- Drift/TagCell.swift
+++ Drift/TagCell.swift
@@ -19,7 +19,7 @@ public class TagCell: UITableViewCell, UITextFieldDelegate {
     @IBOutlet var frontView: UIView?
     @IBOutlet var frontViewLeadingConstraint: NSLayoutConstraint?
     @IBOutlet var frontViewTrailingConstraint: NSLayoutConstraint?
-    
+
     @IBOutlet var backViewToTagNameTextFieldConstraint: NSLayoutConstraint?
     @IBOutlet var frontViewLeftSeparatorConstraint: NSLayoutConstraint?
     @IBOutlet var rightSelected: UIView?
@@ -40,18 +40,18 @@ public class TagCell: UITableViewCell, UITextFieldDelegate {
             animation.fromValue = self.rightSelected?.backgroundColor?.CGColor
             animation.toValue = backgroundColor.CGColor
             animation.duration = 0.4
-            
+
             self.rightSelected?.layer.addAnimation(animation, forKey: "backgroundColor")
         }
-        
+
         self.rightSelected?.layer.backgroundColor = backgroundColor.CGColor
     }
-    
+
     override public func prepareForReuse() {
         self.frontViewLeadingConstraint?.constant = 0
         self.frontViewTrailingConstraint?.constant = 0
     }
-    
+
     // Public methods
     public func setTitle(title: String?) {
         if let t = title? {
@@ -64,7 +64,7 @@ public class TagCell: UITableViewCell, UITextFieldDelegate {
             self.tagName?.textColor = UIColor(white: 0.267, alpha: 0.4)
         }
     }
-    
+
     // IBActions
     @IBAction func deleteTag(sender: UIButton, forEvent event: UIEvent) {
         self.delegate?.didDeleteTagCell(self)
@@ -72,21 +72,20 @@ public class TagCell: UITableViewCell, UITextFieldDelegate {
 }
 
 typealias TextFieldDelegate = TagCell
-
 extension TextFieldDelegate {
     public func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
 
         return true
     }
-    
+
     public func textFieldShouldEndEditing(textField: UITextField) -> Bool {
         return true
     }
-    
+
     public func textFieldDidEndEditing(textField: UITextField) {
         textField.resignFirstResponder()
-        
+
         self.delegate?.didEditTagCell(self)
     }
 }
