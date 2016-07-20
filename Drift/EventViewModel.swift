77b2413afeff48d01855863f4c23954d3b01f137
Mikael Hultgren <blomma@gmail.com>
2016-07-20 15:51:19 +0200

    Added viewModel layer for EventViewController Non optional viewModel
    By all accounts it makes far more sense to have a non optional
    viewModel than to have a optional one

---
+++ Drift/EventViewModel.swift
@@ -0,0 +1,246 @@
+import Foundation
+
+struct StartComponents {
+	let time: String
+	let day: String
+	let month: String
+	let year: String
+
+	init(time: String = "", day: String = "", month: String = "", year: String = "") {
+		self.time = time
+		self.day = day
+		self.month = month
+		self.year = year
+	}
+}
+
+struct StopComponents {
+	let time: String
+	let day: String
+	let month: String
+	let year: String
+
+	init(time: String = "", day: String = "", month: String = "", year: String = "") {
+		self.time = time
+		self.day = day
+		self.month = month
+		self.year = year
+	}
+}
+
+struct RunningComponents {
+	let minute: String
+	let hour: String
+
+	init(minute: String = "", hour: String = "") {
+		self.minute = minute
+		self.hour = hour
+	}
+}
+
+class EventViewModel: CoreDataInjected {
+	private var startDate: Date?
+	var start: StartComponents = StartComponents() {
+		didSet {
+			startDidUpdate?(start: start)
+		}
+	}
+	var startDidUpdate: ((start: StartComponents) -> Void)?
+
+
+	var stopDate: Date?
+	var stop: StopComponents = StopComponents() {
+		didSet {
+			stopDidUpdate?(stop: stop)
+		}
+	}
+	var stopDidUpdate: ((stop: StopComponents) -> Void)?
+
+
+	private var running: RunningComponents = RunningComponents() {
+		didSet {
+			runningDidUpdate?(running: running)
+		}
+	}
+	var runningDidUpdate: ((running: RunningComponents) -> Void)?
+
+
+	var tag: String? {
+		didSet {
+			tagDidUpdate?(tag: tag)
+		}
+	}
+	var tagDidUpdate: ((tag: String?) -> Void)?
+
+
+	var isRunning: Bool = false {
+		didSet {
+			isRunningDidUpdate?(isRunning: isRunning, startDate: startDate, stopDate: stopDate)
+		}
+	}
+	var isRunningDidUpdate: ((isRunning: Bool, startDate: Date?, stopDate: Date?) -> Void)?
+
+
+	private let calendar = Calendar.autoupdatingCurrent
+	private let shortStandaloneMonthSymbols: Array = DateFormatter().shortStandaloneMonthSymbols
+
+	var selectedEventID: URL?
+
+	func setup() {
+		if let url = State().selectedEventID {
+			let result: Result<Event, FetchError> = fetch(url: url, inContext: persistentContainer.viewContext)
+			do {
+				let event = try result.dematerialize()
+
+				updateStart(with: event.startDate)
+				updateStop(with: event.stopDate)
+
+				selectedEventID = url
+
+				tag = event.inTag?.name
+
+				isRunning = event.stopDate == nil
+			} catch {
+				// TODO: Error handling
+			}
+		} else {
+			updateStart(with: nil)
+			updateStop(with: nil)
+
+			selectedEventID = nil
+
+			tag = nil
+
+			isRunning = false
+		}
+	}
+
+	func startEvent() {
+		let event = Event(inContext: persistentContainer.viewContext)
+		event.startDate = Date()
+
+		// TODO: Only needed to get permanent object id, maybe a better way to do this
+		do {
+			try save(context: persistentContainer.viewContext)
+		} catch {
+			// TODO: Error handling
+		}
+
+		selectedEventID = event.objectID.uriRepresentation()
+
+		updateStart(with: event.startDate)
+		updateStop(with: event.stopDate)
+
+		tag = nil
+
+		isRunning = true
+	}
+
+	func stopEvent() {
+		// We have a running event
+		guard let id = selectedEventID else {
+			// TODO Error handling
+			fatalError("Missing selectedEventID")
+		}
+
+		let result: Result<Event, FetchError> = fetch(url: id, inContext: persistentContainer.viewContext)
+
+		guard let event = result.value() else {
+			// For some reason there was no event
+			// attached to this objectid, not at all what
+			// was exspected
+			// TODO: Error handling
+			fatalError()
+		}
+
+		event.stopDate = Date()
+		updateStop(with: event.stopDate)
+
+		isRunning = false
+	}
+
+	func updateStart(with date: Date?) {
+		startDate = date
+
+		guard let date = date else {
+			start = StartComponents()
+			return
+		}
+
+		let unitFlags: Calendar.Unit = [.year, .month, .day, .hour, .minute]
+		let components: DateComponents = calendar.components(unitFlags, from: date)
+		if let minute = components.minute,
+			let hour = components.hour,
+			let day = components.day,
+			let month = components.month,
+			let year = components.year
+		{
+			let index = month - 1
+			let shortMonth = shortStandaloneMonthSymbols[index]
+
+			start = StartComponents(
+				time: String(format: "%02ld:%02ld", hour, minute),
+				day: String(format: "%02ld", day),
+				month: shortMonth,
+				year: String(format: "%04ld", year))
+		}
+
+		let runningDate: Date = stopDate != nil ? stopDate! : Date()
+		updateRunning(with: runningDate)
+	}
+
+	func updateStop(with date: Date?) {
+		stopDate = date
+
+		guard let date = date else {
+			stop = StopComponents()
+			return
+		}
+
+		let unitFlags: Calendar.Unit = [.year, .month, .day, .hour, .minute]
+		let components: DateComponents = calendar.components(unitFlags, from: date)
+		if let minute = components.minute,
+			let hour = components.hour,
+			let day = components.day,
+			let month = components.month,
+			let year = components.year
+		{
+			let index = month - 1
+			let shortMonth = shortStandaloneMonthSymbols[index]
+
+			stop = StopComponents(
+				time: String(format: "%02ld:%02ld", hour, minute),
+				day: String(format: "%02ld", day),
+				month: shortMonth,
+				year: String(format: "%04ld", year))
+		}
+
+		updateRunning(with: date)
+	}
+
+	func updateRunning(with date: Date) {
+		guard let startDate = startDate else {
+			// We were called without a startDate
+			// this is never right
+			// TODO: Error handling
+			fatalError()
+		}
+
+		let unitFlags: Calendar.Unit = [.hour, .minute]
+
+		let components: DateComponents = calendar.components(unitFlags, from: startDate, to: date)
+
+		if let minute = components.minute,
+			let hour = components.hour
+		{
+			var runningHours: String = String(format:"%02ld", hour)
+			if hour < 0 || minute < 0 {
+				runningHours = String(format:"-%@", runningHours)
+			}
+
+			running = RunningComponents(
+				minute: String(format:"%02ld", minute),
+				hour: runningHours)
+		}
+	}
+}
--- stray/EventViewController.swift
+++ stray/EventViewController.swift
@@ -1,7 +1,6 @@
 import UIKit
-import CoreData
 
-class EventViewController: UIViewController, EventTimerControlDelegate, CoreDataInjected {
+class EventViewController: UIViewController {
     // MARK: IBOutlet
     @IBOutlet weak var eventTimerControl: EventTimerControl?
     @IBOutlet weak var toggleStartStopButton: UIButton?
@@ -22,121 +21,92 @@ class EventViewController: UIViewController, EventTimerControlDelegate, CoreData
     @IBOutlet weak var tag: UIButton?
 
     // MARK: Private properties
-    private var selectedEventID: URL?
-
-    private let state = State()
     private var transitionOperator: TransitionOperator?
 
-    private let calendar = Calendar.autoupdatingCurrent
-    private let shortStandaloneMonthSymbols: NSArray = DateFormatter().shortStandaloneMonthSymbols
+	var modelView: EventViewModel = EventViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.transitionOperator = TransitionOperator(viewController: self)
+//        self.transitionOperator = TransitionOperator(viewController: self)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        eventTimerControl?.delegate = self
+		let startDidUpdate: (start: StartComponents) -> Void = { [unowned self]
+			(start: StartComponents) in
 
-        var tagName: String? = nil
-        if let url = state.selectedEventID {
-			let result: Result<Event, FetchError> = fetch(url: url, inContext: persistentContainer.viewContext)
-			do {
-				let event = try result.dematerialize()
-				selectedEventID = url
+			self.eventStartTime?.text = start.time
+			self.eventStartDay?.text  = start.day
+			self.eventStartYear?.text = start.year
+			self.eventStartMonth?.text = start.month
+		}
 
-				tagName = event.inTag?.name
-				eventTimerControl?.initWithStart(event.startDate, andStop: event.stopDate)
-				
-				if let _ = event.stopDate {
-					toggleStartStopButton?.setTitle("START", for: UIControlState())
-					animateStopEvent()
-				} else {
-					toggleStartStopButton?.setTitle("STOP", for: UIControlState())
-					animateStartEvent()
-				}
-			} catch {
-				// TODO: Error handling
+		let stopDidUpdate: (stop: StopComponents) -> Void = { [unowned self]
+			(stop: StopComponents) in
+
+			self.eventStopTime?.text = stop.time
+			self.eventStopDay?.text  = stop.day
+			self.eventStopYear?.text = stop.year
+			self.eventStopMonth?.text = stop.month
+		}
+
+		let runningDidUpdate: (running: RunningComponents) -> Void = { [unowned self]
+			(running: RunningComponents) in
+
+			self.eventTimeHours?.text = running.hour
+			self.eventTimeMinutes?.text = running.minute
+		}
+
+		let tagDidUpdate: (tag: String?) -> Void = { [unowned self]
+			(tag: String?) in
+
+			if let tag = tag,
+				let font = UIFont(name: "Helvetica Neue", size: 14) {
+				let attriString = AttributedString(string:tag, attributes: [NSFontAttributeName: font])
+				self.tag?.setAttributedTitle(attriString, for: UIControlState())
+			} else if let font = UIFont(name: "FontAwesome", size: 20) {
+				let attriString = AttributedString(string:"\u{f02b}", attributes: [NSFontAttributeName: font])
+				self.tag?.setAttributedTitle(attriString, for: UIControlState())
 			}
-        } else {
-            selectedEventID = nil
-        }
+		}
 
-        if let tagName = tagName,
-            let font = UIFont(name: "Helvetica Neue", size: 14) {
-                let attriString = AttributedString(string:tagName, attributes:
-                    [NSFontAttributeName: font])
+		let isRunningDidUpdate: (isRunning: Bool, startDate: Date?, stopDate: Date?) -> Void = { [unowned self]
+			(isRunning: Bool, startDate: Date?, stopDate: Date?) in
 
-                tag?.setAttributedTitle(attriString, for: UIControlState())
-        } else if let font = UIFont(name: "FontAwesome", size: 20) {
-            let attriString = AttributedString(string:"\u{f02b}", attributes:
-                [NSFontAttributeName: font])
+			if isRunning {
+				self.eventTimerControl?.initWithStart(startDate, andStop: stopDate)
+				self.toggleStartStopButton?.setTitle("STOP", for: UIControlState())
+				self.animateStartEvent()
+			} else {
+				self.eventTimerControl?.stop()
+				self.toggleStartStopButton?.setTitle("START", for: UIControlState())
+				self.animateStopEvent()
+			}
+		}
 
-            tag?.setAttributedTitle(attriString, for: UIControlState())
-        }
+		modelView.startDidUpdate = startDidUpdate
+		modelView.stopDidUpdate = stopDidUpdate
+		modelView.runningDidUpdate = runningDidUpdate
+		modelView.tagDidUpdate = tagDidUpdate
+		modelView.isRunningDidUpdate = isRunningDidUpdate
+
+		modelView.setup()
+
+		eventTimerControl?.delegate = self
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
-		eventTimerControl?.delegate = nil
-		eventTimerControl?.stop()
     }
 
 	override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
 		if segue.identifier == "segueToTagsFromEvent",
-			let controller = segue.destinationViewController as? TagsViewController,
-            let id = selectedEventID {
-				controller.eventID = id
+			let controller = segue.destinationViewController as? TagsViewController {
+			controller.eventID = modelView.selectedEventID
 		}
 	}
-
-    private func updateStartLabelWithDate(_ date: Date) {
-        let unitFlags: Calendar.Unit = [.year, .month, .day, .hour, .minute]
-
-        let components: DateComponents = calendar.components(unitFlags, from: date)
-
-        eventStartTime?.text  = String(format: "%02ld:%02ld", components.hour!, components.minute!)
-        eventStartDay?.text  = String(format: "%02ld", components.day!)
-        eventStartYear?.text  = String(format: "%04ld", components.year!)
-        let index = components.month! - 1
-        if let month = shortStandaloneMonthSymbols.object(at: index) as? String {
-            eventStartMonth?.text  = month
-        }
-    }
-
-    private func updateEventTimeFromDate(_ fromDate: Date, toDate: Date) {
-        let unitFlags: Calendar.Unit = [.hour, .minute]
-        let components: DateComponents = calendar.components(unitFlags, from: fromDate, to: toDate, options: [])
-
-        let hour: Int = abs(components.hour!)
-        let minute: Int = abs(components.minute!)
-
-        var timeHours: String = String(format:"%02ld", hour)
-        if components.hour < 0 || components.minute < 0 {
-            timeHours = String(format:"-%@", timeHours)
-        }
-
-        eventTimeHours?.text   = timeHours
-        eventTimeMinutes?.text = String(format:"%02ld", minute)
-    }
-
-    private func updateStopLabelWithDate(_ date: Date) {
-        let unitFlags: Calendar.Unit = [.year, .month, .day, .hour, .minute]
-
-        let components: DateComponents = calendar.components(unitFlags, from: date)
-
-        eventStopTime?.text  = String(format: "%02ld:%02ld", components.hour!, components.minute!)
-        eventStopDay?.text  = String(format: "%02ld", components.day!)
-        eventStopYear?.text  = String(format: "%04ld", components.year!)
-        let index = components.month! - 1
-        if let month = shortStandaloneMonthSymbols.object(at: index) as? String {
-            eventStopMonth?.text  = month
-        }
-    }
 
     private func animateStartEvent() {
         UIView.animate(withDuration: 0.3, delay: 0.3, options: .curveEaseIn, animations: { () -> Void in
@@ -166,247 +136,45 @@ class EventViewController: UIViewController, EventTimerControlDelegate, CoreData
             }, completion: nil)
     }
 
-    private func animateEventTransforming(_ eventTimerTransformingEnum: EventTimerTransformingEnum) {
-//        UIView.animate(withDuration: 0.3, delay: 0.3, options: .curveEaseIn, animations: { () -> Void in
-//            var eventStartAlpha: CGFloat = 1
-//            var eventStopAlpha: CGFloat = 1
-//            var eventTimeAlpha: CGFloat = 1
-//            var eventStartMonthYearAlpha: CGFloat = 1
-//            var eventStopMonthYearAlpha: CGFloat = 1
-//
-//            switch eventTimerTransformingEnum {
-//            case .startDateTransformingStart:
-//                eventStartAlpha = 1
-//                eventStartMonthYearAlpha = 1
-//
-//                eventStopAlpha = 0.2
-//                eventStopMonthYearAlpha = 0.2
-//
-//                eventTimeAlpha = 0.2
-//            case .startDateTransformingStop:
-//                if let _ = self.selectedEvent(self.selectedEventGuid)?.stopDate {
-//                    eventStartAlpha = 0.2
-//                    eventStopAlpha = 1
-//                } else {
-//                    eventStartAlpha = 1
-//                    eventStopAlpha = 0.2
-//                }
-//
-//                eventStartMonthYearAlpha = 1
-//                eventStopMonthYearAlpha = 1
-//
-//                eventTimeAlpha = 1
-//            case .nowDateTransformingStart:
-//                eventStartAlpha = 0.2
-//                eventStartMonthYearAlpha = 0.2
-//
-//                eventStopAlpha = 1
-//                eventStopMonthYearAlpha = 1
-//
-//                eventTimeAlpha = 0.2
-//            case .nowDateTransformingStop:
-//                if let _ = self.selectedEvent(self.selectedEventGuid)?.stopDate {
-//                    eventStartAlpha = 0.2
-//                    eventStopAlpha = 1
-//                } else {
-//                    eventStartAlpha = 1
-//                    eventStopAlpha = 0.2
-//                }
-//
-//                eventStartMonthYearAlpha = 1
-//                eventStopMonthYearAlpha = 1
-//
-//                eventTimeAlpha = 1
-//            default:
-//                break
-//            }
-//
-//            self.eventStartDay?.alpha = eventStartAlpha
-//            self.eventStartMonth?.alpha = eventStartMonthYearAlpha
-//            self.eventStartTime?.alpha = eventStartAlpha
-//            self.eventStartYear?.alpha = eventStartMonthYearAlpha
-//
-//            self.eventStopDay?.alpha = eventStopAlpha
-//            self.eventStopMonth?.alpha = eventStopMonthYearAlpha
-//            self.eventStopTime?.alpha = eventStopAlpha
-//            self.eventStopYear?.alpha = eventStopMonthYearAlpha
-//
-//            self.eventTimeHours?.alpha = eventTimeAlpha
-//            self.eventTimeMinutes?.alpha = eventTimeAlpha
-//
-//            }, completion: nil)
-    }
-
 	@IBAction func prepareForUnwind(_ sender: UIStoryboardSegue) {
 		DLog()
 	}
 
     @IBAction func showTags(_ sender: UIButton) {
-        if selectedEventID != nil {
+        if modelView.selectedEventID != nil {
             navigationController?.delegate = nil
             performSegue(withIdentifier: "segueToTagsFromEvent", sender: self)
         }
     }
 
     @IBAction func toggleEventTouchUpInside(_ sender: UIButton) {
-        if let id = selectedEventID {
-			let result: Result<Event, FetchError> = fetch(url: id, inContext: persistentContainer.viewContext)
-
-			guard let event = result.value() else {
-				// For some reason there was no event
-				// attached to this objectid, not at all what
-				// was exspected
-				// TODO: Error handling
-				fatalError()
-			}
-
-            if let _ = event.stopDate {
-				// Event is stoped, so start a new
-				let event = Event(inContext: persistentContainer.viewContext)
-				event.startDate = Date()
-
-				do {
-					try save(context: persistentContainer.viewContext)
-				} catch {
-					// TODO: Error handling
-					print("*** ERROR: [\(#line)] \(#function) \(error)")
-				}
-
-                selectedEventID = event.objectID.uriRepresentation()
-				state.selectedEventID = selectedEventID
-
-				eventTimerControl?.initWithStart(event.startDate, andStop: event.stopDate)
-
-				toggleStartStopButton?.setTitle("STOP", for: UIControlState())
-
-				animateStartEvent()
-
-				if let font = UIFont(name: "FontAwesome", size: 20) {
-					let attriString = AttributedString(string:"\u{f02b}", attributes:
-						[NSFontAttributeName: font])
-
-					tag?.setAttributedTitle(attriString, for: UIControlState())
-				}
-            } else {
-                // Event is started
-                eventTimerControl?.stop()
-                event.stopDate = eventTimerControl?.nowDate
-
-                toggleStartStopButton?.setTitle("START", for: UIControlState())
-                animateStopEvent()
-            }
-        } else {
-			// No event exists, start a new
-			// Event is stoped, so start a new
-			let event = Event(inContext: persistentContainer.viewContext)
-			event.startDate = Date()
-
-			// NOTE: Only needed to get permanent object id
-			do {
-				try save(context: persistentContainer.viewContext)
-			} catch {
-				// TODO: Error handling
-				print("*** ERROR: [\(#line)] \(#function) \(error)")
-			}
-
-            selectedEventID = event.objectID.uriRepresentation()
-            state.selectedEventID = selectedEventID
-
-			eventTimerControl?.initWithStart(event.startDate, andStop: event.stopDate)
-
-			toggleStartStopButton?.setTitle("STOP", for: UIControlState())
-
-			animateStartEvent()
-
-			if let font = UIFont(name: "FontAwesome", size: 20) {
-				let attriString = AttributedString(string:"\u{f02b}", attributes:
-					[NSFontAttributeName: font])
-
-				tag?.setAttributedTitle(attriString, for: UIControlState())
-			}
-		}
-
-		do {
-			try save(context: persistentContainer.viewContext)
-		} catch {
-			// TODO: Error handling
-			print("*** ERROR: [\(#line)] \(#function) \(error)")
+		if modelView.isRunning {
+			modelView.stopEvent()
+		} else {
+			modelView.startEvent()
 		}
     }
 }
 
 // MARK: - EventTimerControlDelegate
-extension EventViewController {
-    func startDateDidUpdate(_ startDate: Date!) {
-		updateStartLabelWithDate(startDate)
-
-		if let toDate = eventTimerControl?.nowDate {
-			updateEventTimeFromDate(startDate, toDate: toDate)
-		}
-    }
-
-    func nowDateDidUpdate(_ nowDate: Date!) {
-		updateStopLabelWithDate(nowDate)
-
-		if let fromDate = eventTimerControl?.startDate {
-			updateEventTimeFromDate(fromDate, toDate: nowDate)
-		}
+extension EventViewController: EventTimerControlDelegate {
+	func nowDateDidUpdate(_ nowDate: Date) {
+		modelView.updateRunning(with: nowDate)
 	}
 
-    func transformingDidUpdate(_ transform: EventTimerTransformingEnum) {
-        switch transform {
-        case .nowDateTransformingStart, .startDateTransformingStart:
-            animateEventTransforming(transform)
-        case .nowDateTransformingStop:
-			guard let id = selectedEventID else {
-				break
-			}
-
-			let result: Result<Event, FetchError> = fetch(url: id, inContext: persistentContainer.viewContext)
-
-			guard let event = result.value() else {
-				// For some reason there was no event
-				// attached to this objectid, not at all what
-				// was exspected
-				// TODO: Error handling
-				break
-			}
-
-            animateEventTransforming(transform)
-            event.stopDate = eventTimerControl?.nowDate
-
-			do {
-				try save(context: persistentContainer.viewContext)
-			} catch {
-				// TODO: Error handling
-				print("*** ERROR: [\(#line)] \(#function) \(error)")
-			}
-        case .startDateTransformingStop:
-            animateEventTransforming(transform)
-            if let startDate = eventTimerControl?.startDate,
-				let id = selectedEventID
-			{
-				let result: Result<Event, FetchError> = fetch(url: id, inContext: persistentContainer.viewContext)
-
-				guard let event = result.value() else {
-					// For some reason there was no event
-					// attached to this objectid, not at all what
-					// was exspected
-					// TODO: Error handling
-					break
-				}
-
-                event.startDate = startDate
-
-				do {
-					try save(context: persistentContainer.viewContext)
-				} catch {
-					// TODO: Error handling
-					print("*** ERROR: [\(#line)] \(#function) \(error)")
-				}
-			}
-        default:
-            break
-        }
-    }
+	func transformingDidUpdate(_ transform: EventTimerTransformingEnum, withStart startDate: Date, andStop stopDate: Date) {
+		switch transform {
+		case .nowDateDidStart, .startDateDidStart:
+			break
+		// animateEventTransforming(transform)
+		case .nowDateDidStop:
+			// animateEventTransforming(transform)
+			modelView.updateStop(with: stopDate)
+		case .startDateDidStop:
+			// animateEventTransforming(transform)
+			modelView.updateStart(with: startDate)
+		default:
+			break
+		}
+	}
 }
